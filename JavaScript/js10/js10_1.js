57092a01c065f20464a1bc4ad490be85dab94f93
suho <suho0815@naver.com>
2023-04-20 12:22:29 +0900

    js fetch 연습

---
+++ JavaScript/js10/js10_1.js
@@ -0,0 +1,21 @@
+const funB = () => {
+    console.log('funB');
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    setTimeout(() => {
+        console.log('A');
+        setTimeout(() => {
+            console.log('B');
+            setTimeout(() => {
+                console.log('C');
+            }, 500);
+        }, 1000);
+    }, 700);
+
+    
+
+    
+    
+});
